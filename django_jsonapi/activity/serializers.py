a2a5fdc519d04d92dca0c6ba58d8744360cf054f
basavaprasad <basavaprasad.m.gola@gmail.com>
2020-06-26 12:06:24 +0530

    Update serializers.py

--- django_jsonapi/activity/serializers.py
+++ django_jsonapi/activity/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework_json_api import serializers
 from activity.models import User, ActivityPeriod
 
-class UserSerializer(serializers.HperlinkedModelSerialilzer):
+class UserSerializer(serializers.ModelSerialilzer):
 	class Meta:
 		model = User
 		fields = ('userid','real_name','tz')
 
-class ActivityPeriodSerializer(serializers.HperlinkedModelSerialilzer):
+class ActivityPeriodSerializer(serializers.ModelSerialilzer):
 	class Meta:
 		model = DishSerializer
 		fields = ('start_time','end_time')
